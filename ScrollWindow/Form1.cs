2741fa7f5c8b46233261aec6163c8e8da2464605
admin <q_dong@qq.com>
2016-10-08 20:12:07 +0800

    22

--- ScrollWindow/Form1.cs
+++ ScrollWindow/Form1.cs
@@ -22,6 +22,7 @@ namespace ScrollWindow
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             //1
+            //2
         }
 
         private void button1_Click(object sender, EventArgs e)
