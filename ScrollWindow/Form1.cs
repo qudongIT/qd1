61651620b9975dca4710c519daa9dbe940f5f202
admin <q_dong@qq.com>
2016-10-08 18:35:56 +0800

    添加项目文件。

---
+++ ScrollWindow/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScrollWindow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //this.Cursor = new Cursor(Application.StartupPath + "\\Cursor1.cur");
+            this.Cursor = new Cursor(@"C:\Users\qd\Documents\Visual Studio 2015\Projects\ScrollWindow\ScrollWindow\Cursor1.cur");
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.ShowDialog();
+        }
+    }
+}
---
+++ ScrollWindow/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScrollWindow
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            //将窗体的背景色设置为白色
+            this.BackColor = Color.White;
+            //当工作区小于300×250像素时显示滚动条
+            this.AutoScrollMinSize = new Size(300, 251);
+            Size t = this.ClientSize;
+        }
+
+        private void Form2_Paint(object sender, PaintEventArgs e)
+        {
+            //base.OnPaint(e);
+            Graphics g = e.Graphics;
+            //平移坐标系
+            g.TranslateTransform(this.AutoScrollPosition.X, this.AutoScrollPosition.Y);
+            //绘制矩形和椭圆
+            g.FillRectangle(Brushes.LightPink, 0, 0, 200, 150);
+            g.FillRectangle(Brushes.LightGreen, 0, 150, 300, 100);
+        }
+    }
+}
